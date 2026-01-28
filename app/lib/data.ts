ce1ae76122e367b6d3d91e57013587a6037dd3ae
Tristochi <tristanlszakacs@gmail.com>
2026-01-28 09:18:22 -0500

    Implements skeleton of table and querying data for main-office inventory page.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,6 +16,10 @@ const sqlConfig: sql.config = {
     password: requireEnv('DB_PASSWORD'),
     database: requireEnv('DB_DATABASE'),
     server: requireEnv('DB_SERVER'),
+    options: {
+      encrypt: false,
+      trustServerCertificate: true
+    }
 };
 const ITEMS_PER_PAGE = 6;
 
@@ -28,6 +32,7 @@ export async function fetchInventoryPages(query: string) {
         request.input('query', sql.VarChar, likeQuery);
 
         const result = await request.query(`
+            use vns_inventory;
             SELECT COUNT(*) AS count
             FROM itemlocation
             JOIN location
@@ -45,7 +50,7 @@ export async function fetchInventoryPages(query: string) {
                 item.item_name LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS OR
                 category.category_name LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS OR
                 UnitOfMeasure.unit_name LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS OR
-                inventory.vendor LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS
+                vendor.vendor_name LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS
         `);
         const totalPages = Math.ceil(Number(result.recordset[0].count) / ITEMS_PER_PAGE);
         return totalPages;
@@ -57,41 +62,51 @@ export async function fetchInventoryPages(query: string) {
 
 export async function fetchFilteredInventory(
   query: string,
-  currentPage: number
+  currentPage: number,
+  location: string
 ): Promise<InventoryTable[]> {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-
+  
   try {
     // ensure connection (uses pool internally)
     const pool = await sql.connect(sqlConfig);
     const request = pool.request();
 
     const likeQuery = `%${query}%`;
-
+    const likeLocation = `${location}`
+    
     request.input('query', sql.VarChar, likeQuery);
     request.input('limit', sql.Int, ITEMS_PER_PAGE);
     request.input('offset', sql.Int, offset);
+    request.input('location', sql.VarChar, likeLocation)
 
     const result = await request.query(`
       SELECT
-        inventory.item_id,
-        inventory.item_name,
-        inventory.category,
-        inventory.unit,
-        inventory.vendor,
-        inventory.quantity
-      FROM inventory
+        itemlocation.item_id,
+        item.item_name,
+        itemlocation.qty_units_on_hand,
+        unitofmeasure.unit_name,
+        vendor.vendor_name,
+        category.category_name
+      FROM itemlocation
+      JOIN item 
+        ON itemlocation.item_id = item.item_id 
+      JOIN location
+        ON itemlocation.location_id = location.location_id
+      JOIN category
+        ON item.category_id = category.category_id
+      JOIN vendor 
+        ON item.vendor_id = vendor.vendor_id 
+      JOIN unitofmeasure
+        ON item.unit_id = unitofmeasure.unit_id 
       WHERE
-        inventory.item_id LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS OR
-        inventory.item_name LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS OR
-        inventory.category LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS OR
-        inventory.unit LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS OR
-        inventory.vendor LIKE @query COLLATE SQL_Latin1_General_CP1_CI_AS
-      ORDER BY inventory.item_id DESC
+        
+        location.location_name = @location COLLATE SQL_Latin1_General_CP1_CI_AS
+      ORDER BY itemlocation.item_id DESC
       OFFSET @offset ROWS
       FETCH NEXT @limit ROWS ONLY
     `);
-
+    await pool.close();  
     return result.recordset;
   } catch (error) {
     console.error('Database Error:', error);
--- app/ui/inventory/table.tsx
+++ app/ui/inventory/table.tsx
@@ -1,14 +1,17 @@
 import { fetchFilteredInventory } from '@/app/lib/data';
+import { Item, Inventory } from '@/app/lib/definitions';
 
 export default async function InventoryTable({
     query,
     currentPage,
+    location,
 }: {
     query: string;
     currentPage: number;
+    location: string;
 }) {
-    const inventory = await fetchFilteredInventory(query, currentPage);
-
+    const inventory = await fetchFilteredInventory(query, currentPage, location);
+    console.log(inventory);
     return (
         <div className="mt-6 flow-root">
             <div className="inline-block min-w-full align-middle">
@@ -16,10 +19,55 @@ export default async function InventoryTable({
                     <div className="md:hidden">
                         {inventory?.map((item) => (
                             <div key={item.item_id} className="mb-2 w-full rounded-md bg-white p-4">
+                                <div className="flex items-center justify-between border-b pb-4">
+                                    <div>
+                                        <p>{item.item_id}</p>
+                                        <p>{item.item_name}</p>
+                                        <p>{item.qty_units_on_hand}</p>
+                                        <p>{item.unit_name}</p>
+                                    </div>
+                                </div>
                                 
                             </div>
                         ))}
                     </div>
+                    <table className="hidden min-w-full text-gray-900 md:table">
+                        <thead className="rounded-lg text-left text-sm font-normal">
+                            <tr>
+                                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                                    Item ID 
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-medium">
+                                    Item Name 
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-medium">
+                                    Item Qty 
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-medium">
+                                    Units 
+                                </th>                                
+                            </tr>
+                        </thead>
+                        <tbody className="bg-white">
+                            {inventory?.map((item) => (
+                                <tr key={item.item_id}
+                                className="w-full boreder-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+                                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                                        <p>{item.item_id}</p>
+                                    </td>
+                                    <td className="whitespace-nowrap px-3 py-3">
+                                        <p>{item.item_name}</p>
+                                    </td>   
+                                    <td className="whitespace-nowrap px-3 py-3">
+                                        <p>{item.qty_units_on_hand}</p>
+                                    </td>
+                                    <td className="whitespace-nowrap px-3 py-3">
+                                        <p>{item.unit_name}</p>
+                                    </td>                                                                          
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
